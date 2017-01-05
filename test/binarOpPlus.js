56cd0803e7782e62eda46d2380bbb39660a5aa88
navneethg <navneeth@backbench.io>
2017-01-06 01:54:27 +0530

    Update test case: bianryOpPlus string + nonString

--- test/binarOpPlus.js
+++ test/binarOpPlus.js
@@ -4,6 +4,8 @@ var _ = require("lodash");
 
 var expect = require("chai").expect;
 
+var BetalBlob = require("../lib/blob/betal");
+
 var NumberBlob = require("../lib/blob/number");
 
 var StringBlob = require("../lib/blob/string");
@@ -46,5 +48,36 @@ describe("binaryOpPlus test", function() {
 
     });
 
+    describe("string + nonString must concatenate string and nonString.stringRep()", function() {
 
-})
+        let leftString = new StringBlob("hello");
+
+        it("string + betal", function() {
+
+            let betals = [Constants.BETAL_NULL, Constants.BETAL_FALSE, Constants.BETAL_TRUE];
+
+            let check = _.map(betals, (betal) =>
+
+                    binaryOpPlus(leftString, betal).stringRep() ===
+
+                        leftString.concat(new StringBlob(betal.stringRep())).stringRep());
+
+        });
+
+        it("string + number", function() { 
+
+            let nums = [0, -1, 3.14, -14.25];
+
+            let check = _.map(nums, (num) =>
+
+                binaryOpPlus(leftString, new NumberBlob(num)).stringRep() === 
+
+                    leftString.concat(new StringBlob(num.toString())).stringRep());
+
+            expect(_.every(check)).to.be.true;
+
+        });
+
+    });
+
+});
