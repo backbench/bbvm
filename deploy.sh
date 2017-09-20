b94c8c5aa11c2235721af98e6b2c7e417e046fc4
peeyushsrj <peeyushsrj@gmail.com>
2017-09-20 14:59:15 +0530

    Logging done

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 gem install dpl
-dpl --provider="lambda" --access_key_id="$AWS_ACCESS_KEY" --secret_access_key="$AWS_SECRET_KEY" --function_name="bbvm-dev" --role="$AWS_LAMBDA_ROLE" --handler_name="handler" --timeout="10" --memory-size="256" --runtime="nodejs4.3";
+dpl --provider="lambda" --access_key_id="$AWS_ACCESS_KEY" --secret_access_key="$AWS_SECRET_KEY" --function_name="bbvm-dev" --role="$AWS_LAMBDA_ROLE" --handler_name="handler" --timeout="10" --memory-size="256" --runtime="nodejs6.10";
